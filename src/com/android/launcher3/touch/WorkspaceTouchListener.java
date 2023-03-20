a4ce6405389fabefba29c9d932331a74a193bca7
Juan <jramos69@gmail.com>
2023-03-19 23:24:50 -0400

    Revert "ArrowLauncher: Double tap on home screen to turn off screen"
    This reverts commit a1b980031c4df1e3a396dcf7a063465ea88f830d.

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -26,10 +26,8 @@ import static com.android.launcher3.LauncherState.NORMAL;
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_ALLAPPS_CLOSE_TAP_OUTSIDE;
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_WORKSPACE_LONGPRESS;
 
-import android.content.Context;
 import android.graphics.PointF;
 import android.graphics.Rect;
-import android.os.PowerManager;
 import android.view.GestureDetector;
 import android.view.HapticFeedbackConstants;
 import android.view.MotionEvent;
@@ -72,8 +70,6 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
     private final float mTouchSlop;
 
     private int mLongPressState = STATE_CANCELLED;
-    
-    private final PowerManager mPm;
 
     private final GestureDetector mGestureDetector;
 
@@ -83,7 +79,6 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
         // Use twice the touch slop as we are looking for long press which is more
         // likely to cause movement.
         mTouchSlop = 2 * ViewConfiguration.get(launcher).getScaledTouchSlop();
-        mPm = (PowerManager) workspace.getContext().getSystemService(Context.POWER_SERVICE);
         mGestureDetector = new GestureDetector(workspace.getContext(), this);
     }
 
@@ -214,11 +209,5 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
                 cancelLongPress();
             }
         }
-    }
-    
-    @Override
-    public boolean onDoubleTap(MotionEvent event) {
-        mPm.goToSleep(event.getEventTime());
-        return true;
     }
 }
